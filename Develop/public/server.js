9a3270a2aa624302dba5828e83d5d6cc32f79b1c
jeffmlb91 <jeffmlb91@gmail.com>
2021-11-14 23:24:44 -0500

    add API routes GET, POST & DELETE

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -19,3 +19,20 @@ app.use(express.json());
 // adding the static Middleware
 
 app.use(express.static("public"));
+
+//Routing API : GET request
+app.get('api/notes', function(req, res) {
+
+});
+
+//Routing API : POST request
+
+app.post('api/notes', function(req, res) {
+
+});
+
+//Routing API : DELETE request
+
+app.delete('api/notes', function(req, res) {
+
+});
